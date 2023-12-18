6dd237a7e6dd7915a0c52f5c2cfcb8f67f63347e
Anthony Tsui <68526943+s3791898@users.noreply.github.com>
2023-12-18 16:23:40 +1100

    Added the functionality to change the grid size
    Users can now generate a new grid size from 16 - 100 squares per side. The colour change is also now randomised instead of just changing the cells from white to black.

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ sketchArea.style.height = `${GRIDSIDE}px`;
 
 // Function to create grid cells
 function createGridCells() {
+  // Remove existing grid cells
+  while (sketchArea.firstChild) {
+    sketchArea.removeChild(sketchArea.firstChild);
+  }
+
   // Calculate total number of squares
   const numOfSquares = squaresPerSide * squaresPerSide;
   // Calculate width or height of each grid cell
@@ -25,15 +30,58 @@ function createGridCells() {
     // Add a class to the grid cell for styling purposes
     gridCell.classList.add("cell");
 
-    // Add a mouseover event listener to change the background colour to black
-    gridCell.addEventListener("mouseover", () => {
-      gridCell.style.backgroundColor = "black";
-    });
+    // Add mousedown event to start drawing
+    gridCell.addEventListener("mousedown", startDrawing);
 
     // Append the grid cell to the sketch area
     sketchArea.appendChild(gridCell);
   }
 }
 
+function startDrawing(event) {
+  // Change the background colour when drawing
+  event.target.style.backgroundColor = generateRandomColour();
+
+  // Add mousemove and mouseup event listeners
+  document.addEventListener("mousemove", draw);
+  document.addEventListener("mouseup", stopDrawing);
+}
+
+function draw(event) {
+  // Change the background colour while moving the mouse during drawing
+  if (event.target.classList.contains("cell")) {
+    event.target.style.backgroundColor = generateRandomColour();
+  }
+}
+
+function stopDrawing() {
+  // Remove mousemove and mouseup event listeners to stop drawing
+  document.removeEventListener("mousemove", draw);
+  document.removeEventListener("mouseup", stopDrawing);
+}
+
+function generateRandomColour() {
+  // Generates random rgb values and returns it in the correct format
+  const randomR = Math.floor(Math.random() * 256);
+  const randomG = Math.floor(Math.random() * 256);
+  const randomB = Math.floor(Math.random() * 256);
+  return `rgb(${randomR}, ${randomG}, ${randomB})`;
+}
+
 // Call the function to create grid cells
 createGridCells();
+
+function promptForNewSize() {
+  let gridSize = Number(
+    prompt("Enter the number of squares per side for the new grid")
+  );
+
+  if (gridSize >= 16 && gridSize <= 100) {
+    squaresPerSide = gridSize;
+    createGridCells();
+  } else if (gridSize <= 0) {
+    alert("Please enter a positive number");
+  } else {
+    alert("Please enter a size between 16 - 100");
+  }
+}
